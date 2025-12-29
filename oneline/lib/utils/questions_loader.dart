7259d445352c337caf18c53ffb40ff2385cbf1e8
abdalla980 <abdullahizzldin1@gmail.com>
2025-12-29 02:09:32 +0100

    your message

--- oneline/lib/utils/questions_loader.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:convert';
-import 'package:flutter/services.dart' show rootBundle;
-
-Future<Map<String, dynamic>> loadQuestions() async {
-  final jsonStr = await rootBundle.loadString('questions/questions.json');
-  return json.decode(jsonStr) as Map<String, dynamic>;
-}
-
